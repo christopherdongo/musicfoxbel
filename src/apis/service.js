b0b4356bdf00e4eaedec370e99bc1dd503d477db
darkzero <darkzero@localhost.localdomain>
2021-02-21 00:22:43 -0500

    arreglo de las mensajes de error

--- src/apis/service.js
+++ src/apis/service.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-//const backend ='https://foxbelmusicserver.herokuapp.com/'
+const backend ='https://foxbelmusicserver.herokuapp.com/'
 
 //api de signin
 const clienteAxios2 = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: backend
 })
 
 export default clienteAxios2
